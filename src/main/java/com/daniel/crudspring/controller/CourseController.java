8b334ffd0f2a436c24c2e4015cba0ba2d197e883
Daniel R Gorgonha <rogergorgonha@gmail.com>
2023-04-02 20:07:31 -0300

    feat: add endpoint  to course listing

---
+++ src/main/java/com/daniel/crudspring/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.daniel.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.crudspring.model.Course;
+import com.daniel.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+  private final CourseRepository courseRepository;
+
+  @GetMapping
+  public List<Course> list() {
+    return courseRepository.findAll();
+  }
+}
---
+++ src/main/java/com/daniel/crudspring/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.daniel.crudspring.repository;
+
+import com.daniel.crudspring.model.Course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+  
+}
